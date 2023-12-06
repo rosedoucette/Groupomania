603a150e691eb040a7142207aa6087cc7421eebe
rosedoucette <rose.doucette94@gmail.com>
2023-12-06 14:41:47 -0500

    created dropdown menu with links
    link to: profile, logout, delete account

--- front/src/components/topbar/Topbar.jsx
+++ front/src/components/topbar/Topbar.jsx
@@ -1,13 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./topbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { FaBell, FaComment, FaSearch, FaUser } from "react-icons/fa";
+import axios from "axios";
 
 export default function Topbar() {
 
-  const { user } = useContext(AuthContext)
+  const { user, dispatch } = useContext(AuthContext)
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const navigate = useNavigate(); 
+
+  const handleLogout = () => {
+    // Perform logout logic, e.g., clearing user data from local storage and updating context
+    dispatch({ type: "LOGOUT" });
+    navigate("/login");
+  };
+
+  const handleClick = (e) => {
+    e.stopPropagation(); // Stop the event from bubbling up and triggering the Link for profile and for the dropdown
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleDeleteAccount = async () => {
+    try {
+      // Make a delete request using Axios
+      await axios.delete(`/api/users/${user._id}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_YOUR_AUTH_TOKEN}`, // Replace with your actual authentication token
+        },
+      });
+
+      // After successful deletion, perform logout
+      handleLogout();
+    } catch (error) {
+      console.error("Error deleting account:", error);
+    }
+  };
+
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -39,13 +70,28 @@ export default function Topbar() {
           <span className="topbarIconBadge">1</span>
         </div>
       </div>
-      <Link to={`/profile/${user?.username}`}>
+      <div className="topbarProfile" onClick={handleClick}>
+      {/* <Link to={`/profile/${user?.username}`}> */}
         <img src={
           user?.profilePicture
             ? "//localhost:3000/api/images/" + user.profilePicture
             : "//localhost:3000/api/images/avatar.png"}
           alt="" className="topbarImg" />
-      </Link>
+      {/* </Link> */}
+      {dropdownVisible && (
+          <div className="profileDropdown">
+            <Link to={`/profile/${user?.username}`} className="dropdownItem">
+              Profile
+            </Link>
+            <span className="dropdownItem" onClick={handleLogout}>
+              Logout
+            </span>
+            <span className="dropdownItem" onClick={handleDeleteAccount}>
+              Delete Account
+            </span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
